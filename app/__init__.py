1e9ce98ed169210bc459ada94b2d842f386f4315
william-tu <a1023122541@qq.com>
2017-05-20 19:06:18 +0800

    添加搜索接口，优化接口

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,21 @@
-from flask import Flask,Blueprint
+from flask import Flask
 from flask_mongoengine import MongoEngine
-
+from elasticsearch import Elasticsearch
 
 
 from config import config
 
 
 db = MongoEngine()
-
+client = Elasticsearch()
 
 def create_app(config_name):
 
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     db.init_app(app)
+
+
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 from flask import request,current_app,jsonify
 from . import main
-from ..models import Douban
+from ..models import Douban,Guoke
 from ..responses import forbbiden
+from .. import client
 
 
-@main.route('/api/article',methods=['POST'])
-def hot():
-    page = request.json.get('page',1)
+from math import ceil
+
+
+@main.route('/api/douban/article',methods=['GET'])
+def douban():
+    page = request.args.get('page',1,type=int)
     if  not isinstance(page,int):
         return forbbiden('type error')
     pagination = Douban.objects.paginate(page=page,per_page=current_app.config['PER_PAGE'])
@@ -16,3 +20,52 @@ def hot():
         'next': pagination.has_next,
         'pages':pagination.pages
     })
+
+@main.route('/api/guoke/article',methods=['GET'])
+def guoke():
+    page = request.args.get('page',1,type=int)
+    if  not isinstance(page,int):
+        return forbbiden('type error')
+    pagination = Guoke.objects.paginate(page=page,per_page=current_app.config['PER_PAGE'])
+    return jsonify({
+        'article_resource':[ i.to_json() for i in pagination.items],
+        'next': pagination.has_next,
+        'current_page': page,
+        'total_pages':pagination.pages
+    })
+
+@main.route('/api/search',methods=['POST'])
+def search():
+    page = request.json.get('page', 1)
+    if not isinstance(page, int):
+        return forbbiden('type error')
+    query_words = request.json.get('query','')
+    res = client.search(
+        index="douban",
+        body={
+            "query": {
+                "multi_match": {
+                    "query": query_words,
+                    "fields": ["source_from", "title", "content"]
+                }
+            },
+            "from": (page - 1) * current_app.config['PER_PAGE'],
+            "size": current_app.config['PER_PAGE'],
+        }
+    )
+    result = []
+    total = 0
+    if not total:
+        total = res['hits']['total']
+    for hit in res['hits']['hits']:
+        result.append(hit["_source"])
+    return jsonify({
+        'article_resource': result,
+        'next': page * current_app.config['PER_PAGE'] < total,
+        'current_page': page,
+        'total_pages':int(ceil(total/float(current_app.config['PER_PAGE'])))
+
+    })
+
+
+
